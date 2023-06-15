be182796e25b75012fe2e6c4ee470bccd1256dc8
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-06-15 10:30:02 +0300

    "feat(index.ts): add date difference calculation and displaying"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
-import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {useState} from "react";
 import {GetServerSideProps} from "next";
 
 /**
