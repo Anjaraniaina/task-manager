5245cdcef7027a547db5572b3ed76d9fb994910e
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-06-15 11:33:49 +0300

    "feat(useLocalStorage.ts): add custom hook using localStorage"

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,20 @@
-const useLocalStorage = () => {
+import {useState} from "react";
 
+const useLocalStorage = (key: string, initialValue: any) => {
+  const [value, setValue] = useState(()=>{
+    // current stored value
+    const stored = localStorage.getItem(key);
+    // if no stored value, set initialValue
+    return stored !== null ? JSON.parse(stored) : initialValue;
+  });
+
+  // save a new value
+  const setLocalStorageValue = (newValue: any) => {
+    setValue(newValue)
+    localStorage.setItem(key, newValue);
+  }
+
+  return [value, setLocalStorageValue];
 }
 
 export {
