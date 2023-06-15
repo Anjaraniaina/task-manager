cbcfb12470b9d5660b040e2884008f60672bbfbe
Anjaraniaina <hei.anjaraniaina@gmail.com>
2023-06-15 10:28:27 +0300

    "feat(index.ts): add date difference calculation and displaying"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
+import {useEffect, useState} from "react";
+import {GetServerSideProps} from "next";
 
 /**
   Calculates the time difference between the server time and client time.
@@ -7,14 +9,49 @@ import { useRouter } from "next/router";
   @param {Date} clientTime - The client time.
   @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
 */
-const calculateTimeDifference = (server: Date, client: Date) => {};
+const calculateTimeDifference = (serverTime: Date, clientTime: Date) => {
+  let difference = serverTime.getTime() - clientTime.getTime();
+  let days = Math.ceil(difference / (1000 * 3600 * 24));
+  let hours = Math.ceil(difference / (1000 * 3600));
+  let minutes = Math.ceil(difference / (1000 * 60));
+  let seconds = Math.ceil(difference / (1000));
+  return `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds}`;
+};
 
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const currentDate = new Date();
 
-export default function Home() {
+  return {
+    props: {
+      serverTime: currentDate.toISOString(),
+    },
+  };
+};
+
+// create interface to type props
+interface HomeProps {
+  serverTime: string;
+}
+// function to format the date
+const formatDate = (date: Date): string => {
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const year = date.getFullYear();
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+
+  return `${day}-${month}-${year} ${hours}:${minutes}`;
+};
+
+export default function Home({serverTime}: HomeProps) {
+  const [clientTime, setClientTime] = useState(new Date());
   const router = useRouter();
   const moveToTaskManager = () => {
     router.push("/tasks");
   }
+
+  const timeDifference = calculateTimeDifference(new Date(serverTime), clientTime);
+
   return (
     <>
       <Head>
@@ -26,16 +63,15 @@ export default function Home() {
       <main>
         <h1>The easiest exam you will ever find</h1>
         <div>
-          {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{formatDate(new Date(serverTime))}</span>
           </p>
 
-          {/* Display here the time difference between the server side and the client side */}
+          {timeDifference}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{formatDate(clientTime)}</span>
           </p>
         </div>
 
